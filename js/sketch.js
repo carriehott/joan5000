c0f21734814cd60b54147f6de2bff86d567c9eb5
carriehott <carriehott@gmail.com>
2020-04-17 18:07:05 -0700

    new world

---
+++ js/sketch.js
@@ -0,0 +1,35 @@
+var towel;
+var link;
+
+function preload() {
+    towel = loadImage('../img/HangingTowel.png');
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    noCursor();
+}
+
+function draw() {
+    background('#E6FFF4');
+
+    if (mouseX > width / 1.5 && mouseY > height / 1.5) {
+
+        fill('#FF07D6');
+        textSize(35);
+        textFont('Oxanium');
+        text("Here I Am", width / 1.5, height / 1.5);
+    }
+    imageMode(CENTER);
+    image(towel, mouseX, mouseY);
+}
+
+function mousePressed() {
+  if (mouseX > width / 1.5 && mouseY > height / 1.5) {
+    window.location.pathname = '../002.html'
+  }
+  }
