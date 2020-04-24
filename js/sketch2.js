11e59e407613501dff8d580338056129bd767587
carriehott <carriehott@gmail.com>
2020-04-24 15:02:56 -0700

    page 3 start

---
+++ js/sketch2.js
@@ -0,0 +1,35 @@
+var capture;
+
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    //noCursor();
+    capture = createCapture(VIDEO);
+    capture.size(220, 150);
+    capture.hide();
+}
+
+function draw() {
+    background('lightpink');
+    tint('pink');
+    image(capture, width/4, height/4);
+    tint('yellow');
+    image(capture, width/3, height/3);
+    tint('orange');
+    image(capture, width/2, height/2);
+    tint('red');
+    image(capture, width/1.5, height/1.5);
+
+    imageMode(CENTER);
+    image(towel, mouseX, mouseY);
+}
+
+function mousePressed() {
+    if (mouseX > width / 1.5 && mouseY > height / 1.5) {
+        window.location.href = 'https://carriehott.github.io/joan5000/004.html'
+    }
+}
