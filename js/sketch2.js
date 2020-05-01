1b39faea57a9039e24d0b7450a7dff54adab9a48
carriehott <carriehott@gmail.com>
2020-05-01 13:32:44 -0700

    vacuum

--- js/sketch2.js
+++ js/sketch2.js
@@ -1,9 +1,5 @@
 var capture;
-var jar; 
 
-function preload() {
-    jar=loadImage("jar_cursor.png")
-}
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
@@ -11,7 +7,6 @@ function windowResized() {
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    noCursor();
     capture = createCapture(VIDEO);
     capture.size(220, 150);
     capture.hide();
@@ -28,12 +23,4 @@ function draw() {
     tint('red');
     image(capture, width/1.5, height/1.5);
 
-    imageMode(CENTER);
-    image(jar, mouseX, mouseY);
-}
-
-function mousePressed() {
-    if (mouseX > width / 1.5 && mouseY > height / 1.5) {
-        window.location.href = 'https://carriehott.github.io/joan5000/004.html'
-    }
 }
