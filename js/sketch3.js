e72e502416b236f644a96b268f2c403e7ee1df5b
carriehott <carriehott@gmail.com>
2021-02-12 09:25:32 -0800

    that's whats up

--- js/sketch3.js
+++ js/sketch3.js
@@ -1,7 +1,9 @@
 var candle;
+var meeting; 
 
 function preload() {
-    candle=loadImage("img/candle.gif"); 
+    candle = loadImage("img/candle.gif");
+    meeting = loadImage("img/meeting.png");
 }
 
 function windowResized() {
@@ -10,10 +12,25 @@ function windowResized() {
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    noCursor(); 
+    noCursor();
 }
 
 function draw() {
-    imageMode(CENTER); 
-    image(candle, mouseX, mouseY); 
+    background(meeting);
+
+    if (mouseX > width / 1.5 && mouseY > height / 1.5) {
+
+        fill('yellow');
+        textSize(25);
+        textFont('Oxanium');
+        text("come with me....", width / 1.15, height / 1.5);
+    }
+    imageMode(CENTER);
+    image(candle, mouseX, mouseY);
 }
+
+function mousePressed() {
+  if (mouseX > width / 1.15 && mouseY > height / 1.5) {
+    window.location.href = 'https://carriehott.github.io/joan5000/006.html'
+  }
+  }
